fabea5fd81b65fa88e532ee4a04563a3798ea8fd
Andrew Thomson <athomsonguy@gmail.com>
2015-02-23 13:32:33 -0800

    bump version (again).

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mongomgr',
-      version='0.7',
+      version='0.8',
       description='Tool for managing mongodb replica sets.',
       url='https://github.com/Livefyre/mongo-mgr',
       author='andrew thomson',
